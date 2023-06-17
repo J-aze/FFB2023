7c1d6f2c1781054ed9bc1a3d1d11399810087c6b
Alexis Opolka <53085471+alexis-opolka@users.noreply.github.com>
2023-06-17 17:31:34 +0000

    Correction rapide + Abstraction
    Correction afin de pouvoir exécuter sans utiliser python + refactoriser un peu et généraliser le script

--- scripts/Interrupteur/reception_donnees_interrupteur.py
+++ scripts/Interrupteur/reception_donnees_interrupteur.py
@@ -1,13 +1,17 @@
+#!/bin/env python3
+
 import json
 import paho.mqtt.client as mqtt
-# Pour un affichage plus joli format json dans le terminal
-from rich import print as pprint
 import Decodage_interrupteur as dec
 
 # Callback appelée lors de la réception d'un message MQTT
-def on_message(client, userdata, msg):
-    try:
+def on_message(client, userdata, msg, debug=False):
 
+    ### TODO: Voir si ça ne pose pas de problèmes lors du callback
+    if debug:
+        print("(reception_donnes_interrupteur)[on_message]:", client, userdata)
+
+    try:
         # Initialisation de la variable data avec le contenu du message
         data = json.loads(msg.payload)
         # Debug affichage de la data
@@ -30,15 +34,26 @@ def on_message(client, userdata, msg):
         # Ignorer les messages non valides (non JSON)
         pass
 
+### Constantes
+
+#### MQTT Broker
+MQTT_BROKER_ACCESS_KEY = "LA-CLE-ACCES" # La clé d'accès (API) au flux MQTT
+MQTT_BROKER_SERVER = "LE-BROKER-MQTT" # Adresse du Broker MQTT avec une forme `server@ttn`
+MQTT_DEVICE = "APPAREIL" # L'ID du device que l'on écoute, utiliser `+` pour écouter tous les appareils
+MQTT_DEVICE_FLUX = "LE-FLUX" # Soit `up` soit `down`
+
+#### The Things Network
+TTN_SERVER = "SERVEUR-TTN"
+
 # Configuration du client MQTT
 client = mqtt.Client()
 #  assignation username et access key
-client.username_pw_set('iutb-sae204-2023@ttn', 'NNSXS.6QCXZEARX6HYV6BOVY47EXFMV25MIWFAAFNZFTQ.4Z6ZUZGMNVVGVQY4JCTANZWY7YRHCBG4NQVRYYWLJLPK24RMLDXA')
+client.username_pw_set(MQTT_BROKER_SERVER, MQTT_BROKER_ACCESS_KEY)
 # Connection au server TTN
-client.connect('eu1.cloud.thethings.network', 1883)
+client.connect(TTN_SERVER, 1883)
 
 # Suivi de l'appareil mcf-io-02
-client.subscribe('v3/iutb-sae204-2023@ttn/devices/mcf-io-02/up')
+client.subscribe(f'v3/{MQTT_BROKER_SERVER}/devices/{MQTT_DEVICE}/{MQTT_DEVICE_FLUX}')
 
 # Configuration de la callback pour la réception des messages
 client.on_message = on_message
