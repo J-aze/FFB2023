b2f85c3f95fbeae3b9f81ae847f01b58673c17c1
Alexis Opolka <53085471+alexis-opolka@users.noreply.github.com>
2023-06-19 08:33:09 +0200

    Purge unused imports + Removed the mention of the module

--- scripts/Interrupteur/connexion.py
+++ scripts/Interrupteur/connexion.py
@@ -1,8 +1,5 @@
 # Connexion avec influxDB et envoie de données
-import influxdb_client
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
-from typing import Tuple
+from influxdb_client import InfluxDBClient
 
 
 def connexion(INFLUXDB_URL: str, INFLUXDB_ORG: str, INFLUXDB_TOKEN: str) -> InfluxDBClient | bool:
@@ -10,7 +7,7 @@ def connexion(INFLUXDB_URL: str, INFLUXDB_ORG: str, INFLUXDB_TOKEN: str) -> Infl
     # client -> InfluxDBClient
     # Peut faire monter une erreur
     # Si tout s'est bien passé -> retourne (client, code d'exécution)
-    client = influxdb_client.InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+    client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 
     if client.ping():
         # L'exécution s'est bien passée, on retourne le pointeur du client (InfluxDBClient)
