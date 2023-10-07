169bf515dcbeffff850cc60661a35fa23e2e7870
alexis-opolka <alexis.opolka@proton.me>
2023-10-07 08:53:44 +0200

    Update token

--- apps/ffb-server/env.mjs
+++ apps/ffb-server/env.mjs
@@ -1,7 +1,7 @@
 /** InfluxDB v2 URL */
 const url = process.env["INFLUX_URL"] || "https://influxffb.iutbeziers.fr";
 /** InfluxDB authorization token */
-const token = process.env["INFLUX_TOKEN"] || "cyXUzkDCr-mcm8ebSNldN7oLDMOp9os1yORAwIA2b8tK9QpXQ4oCQ5CqxUcAAmGzrUuA0AMUF0UuIO2XxDYbBQ==";
+const token = process.env["INFLUX_TOKEN"] || "BfZyF1yP3Fpa2BXvpXf4tZKy9gPt3UP2NAKNk3PlrgT8d-hEqa1SwyyeoAeuTYEKQrEFOlHrQvPT0fd77cb96Q==";
 /** Organization within InfluxDB  */
 const org = process.env["INFLUX_ORG"] || "FFB2023";
 /**InfluxDB bucket used in examples  */
